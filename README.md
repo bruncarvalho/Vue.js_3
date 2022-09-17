a6cf6b46d99d7c779187fad728453433a0a7db9a
Bruna Carvalho <brunacarvalhocardoso@gmail.com>
2022-09-17 16:05:16 -0300

    declarações condicionais

---
+++ README.md
@@ -0,0 +1,4 @@
+# Iniciando o projeto
+
+* Praticando sobre Declarações condicionais (diretiva v-if v-show)
+
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    header
+  </header>
+    
+  
+</template>
+
+<script>
+  export default {
+   
+  }
+
+</script>
+
+<style>
+ .header{
+  background: #bbb;
+  color: black;
+ }
+</style>
