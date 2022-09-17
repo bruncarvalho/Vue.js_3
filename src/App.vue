aefcf8234bf8cf7d9fc17e5dcabd186dc593600a
Bruna Carvalho <brunacarvalhocardoso@gmail.com>
2022-09-17 20:30:02 -0300

    v-for

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,22 @@
   
   <div id="app">
     <TheHeader
-      v-if="showHeader"
+    
     />
 
-    <div v-show="showName">
-      Nome: {{ firstName }} <br>
-      SobreNome: {{lastName}}
-    </div>
-
-    <div v-if="acessLevel === 'admin'">Usuário Admin</div>
-    <div v-else-if="acessLevel === 'marketing'">Usuário Marketing</div>
-    <div v-else>Usuário normal</div>
-    
-    <img 
-    alt="Vue logo" src="./assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     
   </div>
 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
       TheHeader
-  },
-  data() {
-    return {
-      showHeader: true,
-      firstName: 'Bruna',
-      lastName: 'Carvalho',
-      showName: false,
-      acessLevel: 'marketing'
-    }
   }
 }
 
